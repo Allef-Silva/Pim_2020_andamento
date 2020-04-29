5257120e6d58903e988188bb4fe701cb1a270ba6
Allef Silva <allefmarchetti@hotmail.com>
2020-04-28 22:20:36 -0300

    Mudança dispose tela login

--- PizzariaWinForm/Formularios/frmLogin.Designer.cs
+++ PizzariaWinForm/Formularios/frmLogin.Designer.cs
@@ -77,6 +77,7 @@
             this.button2.Size = new System.Drawing.Size(65, 51);
             this.button2.TabIndex = 11;
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
