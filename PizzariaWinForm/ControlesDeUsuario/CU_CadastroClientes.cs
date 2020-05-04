ec0600127ceaaebc4b6eb934edbf53c9bd06940e
Ademilson <ademilson.aoki@gmai.com>
2020-05-03 21:26:24 -0300

    alterar, tratativas erro

--- PizzariaWinForm/ControlesDeUsuario/CU_CadastroClientes.cs
+++ PizzariaWinForm/ControlesDeUsuario/CU_CadastroClientes.cs
@@ -13,12 +13,14 @@ namespace PizzariaWinForm.ControlesDeUsuario
     public partial class CU_CadastroClientes : UserControl
     {
 
-        
+
         public CU_CadastroClientes()
         {
             InitializeComponent();
 
         }
+    
+
 
 
         private void btnAdicionarFornecedor_Click(object sender, EventArgs e)
@@ -30,9 +32,9 @@ namespace PizzariaWinForm.ControlesDeUsuario
             }
         }
         Cliente cad = new Cliente();
-    
 
- 
+
+
 
 
         private void btnAtulizar_Click(object sender, EventArgs e)
@@ -44,25 +46,36 @@ namespace PizzariaWinForm.ControlesDeUsuario
 
         }
 
+
+       
+
         private void dgvCliente_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            using (Formularios.frmCadastroCliente abn = new Formularios.frmCadastroCliente())
-            {
-                abn.ShowDialog();
-                
 
-                Cliente cli = new Cliente();
-                dgvCliente.CurrentRow.Selected = true;
-                cli.Id =int.Parse(dgvCliente.Rows[e.RowIndex].Cells["Cliente"].FormattedValue.ToString());
-                cli.Nome = dgvCliente.Rows[e.RowIndex].Cells["Nome"].FormattedValue.ToString();
-                cli.Endereco = dgvCliente.Rows[e.RowIndex].Cells["Endereco"].FormattedValue.ToString();
-                cli.Numero =int.Parse( dgvCliente.Rows[e.RowIndex].Cells["Numero"].FormattedValue.ToString());
-                cli.Telefone = dgvCliente.Rows[e.RowIndex].Cells["Telefone"].FormattedValue.ToString();
-                cli.CPF = dgvCliente.Rows[e.RowIndex].Cells["Cpf"].FormattedValue.ToString();
-               
-            }
 
-               
+            string idDgv;
+            string nomeDgv;
+            string enderecoDgv;
+            string numeroDgv;
+            string telefoneDgv;
+            string cpfdDgv;
+            dgvCliente.CurrentRow.Selected = true;
+            idDgv = dgvCliente.Rows[e.RowIndex].Cells["Cliente"].FormattedValue.ToString();
+            nomeDgv = dgvCliente.Rows[e.RowIndex].Cells["Nome"].FormattedValue.ToString();
+            enderecoDgv = dgvCliente.Rows[e.RowIndex].Cells["Endereco"].FormattedValue.ToString();
+            numeroDgv = dgvCliente.Rows[e.RowIndex].Cells["Numero"].FormattedValue.ToString();
+            telefoneDgv = dgvCliente.Rows[e.RowIndex].Cells["Telefone"].FormattedValue.ToString();
+            cpfdDgv = dgvCliente.Rows[e.RowIndex].Cells["Cpf"].FormattedValue.ToString();
+
+
+
+            Formularios.frmCadastroCliente cad = new Formularios.frmCadastroCliente();
+
+            cad.RecebendoValorDgv(idDgv,nomeDgv, enderecoDgv, numeroDgv,telefoneDgv,cpfdDgv);
+            cad.Show();
+
+
         }
+      
     }
 }
